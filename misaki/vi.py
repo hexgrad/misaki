f85ef36f2ac7ab1fc754e68bd2e1ae8e64452dbe
H.D.Tài <tai17082006.tk@gmail.com>
2025-01-27 12:20:00 +0700

    Add option skip_invalid_chars (e.g. Chinese characters)

--- misaki/vi.py
+++ misaki/vi.py
@@ -155,6 +155,7 @@ with importlib.resources.open_text(data, 'vi_teencode.json', encoding = "utf-8")
     TEENCODE_MAPPING = json.load(r)
 NUMBER_REGEX = re.compile(regex_tokenize.number)
 SYMBOL_REGEX = re.compile('|'.join(re.escape(symbol) for symbol in SYMBOL_MAPPING.keys()))
+EN_VI_REGEX = re.compile("^[!-~“”–" + regex_tokenize.VIETNAMESE_VOWELS_LOWER + "]+$")
 
 ################################################3
 
@@ -397,7 +398,8 @@ class ViToken:
 class VIG2P:
     def __init__(self, 
                  glottal = 0, pham = 0, cao = 0, palatals = 0, substr_tokenize = True, dialect = "north",
-                 tone_type = 0, num2words_use_linking_words = True, enable_en_g2p = True, en_g2p_kwargs = {}):
+                 tone_type = 0, num2words_use_linking_words = True, skip_invalid_chars = True,
+                 enable_en_g2p = True, en_g2p_kwargs = {}):
         self.glottal = glottal
         self.pham = pham
         self.cao = cao
@@ -406,6 +408,7 @@ class VIG2P:
         self.delimit = ''
         self.tone_type = tone_type
         self.num2words_use_linking_words = num2words_use_linking_words
+        self.skip_invalid_chars = skip_invalid_chars
         if dialect in ["north", "central", "south"]:
             self.dialect = dialect[0]
         else:
@@ -496,7 +499,7 @@ class VIG2P:
             return compound
 
     def __call__(self, text):
-        TN = TTSnorm(text, self.num2words_use_linking_words)
+        TN = TTSnorm(text, self.num2words_use_linking_words, self.skip_invalid_chars)
         # Words in Vietnamese only have one morphological form, regardless of the compound words
         # so word segmentation is unnecessary
         # E.g. "nhà" trong "nhà xe" and "nhà lầu" are the same /ɲa2/
@@ -504,6 +507,8 @@ class VIG2P:
         IPA = ""
         vitokens: list[ViToken] = []
         for tk in TK:
+            if self.skip_invalid_chars and not EN_VI_REGEX.match(tk):
+                continue
             if tk in ['.', ',', ';', ':', '!', '?', ')', '}', ']']:
                 if tk in [')', '}', ']']:
                     tk = ')'
