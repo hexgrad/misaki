d51cac6856b1cf515129ff65b92a1df0d443b946
Ahmatjan <Hyper-Hack@outlook.com>
2025-02-12 17:52:25 +0800

    Update espeak.py

--- misaki/espeak.py
+++ misaki/espeak.py
@@ -28,7 +28,8 @@ class EspeakFallback:
         # Set espeak-ng library path and espeak-ng-data
         EspeakWrapper.set_library(espeakng_loader.get_library_path())
         # Change data_path as needed when editing espeak-ng phonemes
-        EspeakWrapper.set_data_path(espeakng_loader.get_data_path())
+        # set_data_path is no longer available from the wrapper
+        # EspeakWrapper.set_data_path(espeakng_loader.get_data_path())
         self.backend = phonemizer.backend.EspeakBackend(
             language=f"en-{'gb' if british else 'us'}",
             preserve_punctuation=True, with_stress=True, tie='^'
