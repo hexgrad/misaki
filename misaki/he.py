f0072f381e935d1f4a55a34028c9bbb7bb2597e7
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-06-23 04:33:33 +0300

    feat: add phonikud g2p

--- misaki/he.py
+++ misaki/he.py
@@ -1,22 +1,14 @@
 """
-Phonemize Hebrew using mishkal package from https://github.com/thewh1teagle/mishkal
+Phonemize Hebrew using mishkal package from https://github.com/thewh1teagle/phonikud
 """
 
-import mishkal
+import phonikud
 
 class HEG2P:
-    def __call__(self, text: str, preserve_punctuation = True, preserve_stress = True):
+    def __call__(self, text: str, preserve_punctuation = True, preserve_stress = True, **kwargs):
         """
         Convert Hebrew text to IPA
-        Text is expected to be with diacritics (niqqud)
-        Enable debug to return Word objects that contais detailed conversion information
+        Text is expected to be with enhanced diacritics (nikud)
         """
 
-        return mishkal.phonemize(text, preserve_punctuation=preserve_punctuation, preserve_stress=preserve_stress)
-
-    def get_phonene_set(self):
-        """
-        Return list with exact phonemes used in mishkal package
-        """
-
-        return mishkal.get_phoneme_set()
+        return phonikud.phonemize(text, preserve_punctuation=preserve_punctuation, preserve_stress=preserve_stress, **kwargs)
